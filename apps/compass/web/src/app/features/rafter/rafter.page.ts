d0e0aaf0fd0407b54fec2ca3628d7674120d06c5
Darko Solar <217219045+solariusns@users.noreply.github.com>
2026-09-07 15:51:29 +0200

    feat(web): add RAFTER World page

---
+++ apps/compass/web/src/app/features/rafter/rafter.page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rafter-page',
+  templateUrl: './rafter.page.html',
+  styleUrl: './rafter.page.scss'
+})
+export default class RafterPage {}
